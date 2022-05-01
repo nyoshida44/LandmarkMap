75a9398676f3e43c6664d3c366033aa74002da4d
nyoshida44 <nyoshida44@gmail.com>
2022-05-01 16:16:40 -0400

    Centered text for main page, may need to fix opacity

--- landmark/src/components/Main.jsx
+++ landmark/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Paper } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 import Image from '../img/world.jpg'; 
+import { blue } from '@mui/material/colors';
 
 const styles = {
   paperContainer: {
@@ -10,14 +11,17 @@ const styles = {
     backgroundRepeat: 'no-repeat',
     width: '100vw',
     height: '100vh',
-    opacity: 0.6
+    opacity: 0.5
   }
 };
 
 export const Main = (props) => {
 
   return (
-    <Paper style={styles.paperContainer}>
+    <Paper style={styles.paperContainer} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Typography component="div" variant="h3" color="blue" >
+        Landmark Maker
+      </Typography>
     </Paper>
   );
 }
