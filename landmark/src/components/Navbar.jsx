7aa9fec54f6e52b834a2ccd42dbbf7a86061c3af
nyoshida44 <nyoshida44@gmail.com>
2022-05-01 13:55:12 -0400

    Navbar Fix, Desktop ready

--- landmark/src/components/Navbar.jsx
+++ landmark/src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
-//import { Link } from "react-router-dom";
 
 export const Navbar = (props) => {
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <Box sx={{  display: 'flex', flexDirection: 'row', flexGrow: 1 }}>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Landmark Maker
-          </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography component="div" variant="h6" sx={{ flexGrow: 0.1 }}>
+          Landmark Maker
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography component="div" variant="h7"> 
             Add a Landmark
           </Typography>
         </Box>
-        <Button color="inherit">Logout</Button>
+        <Button color="inherit">
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
